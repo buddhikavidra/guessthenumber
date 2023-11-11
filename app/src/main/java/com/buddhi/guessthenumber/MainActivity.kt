02bfe185c04dd4b50e3384f951eb1d4c1735dae3
buddhikavindra <buddhikavindra02@gmail.com>
2023-11-11 14:51:20 +0530

    changes all

--- app/src/main/java/com/buddhi/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/buddhi/guessthenumber/MainActivity.kt
@@ -11,7 +11,11 @@ import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
 import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import kotlin.math.abs
@@ -33,15 +37,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var totalScoreContainer: TextView
     private lateinit var myRange: SeekBar
     private lateinit var roundScoreContainer: TextView
+    lateinit var mAdView : AdView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequestBanner = AdRequest.Builder().build()
+        mAdView.loadAd(adRequestBanner)
+
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequestbammer = AdRequest.Builder().build()
+        mAdView.loadAd(adRequestbammer)
 
         var adRequest = AdRequest.Builder().build()
-        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+        InterstitialAd.load(this,"ca-app-pub-5582277946065325/6751639431", adRequest, object : InterstitialAdLoadCallback() {
             override fun onAdFailedToLoad(adError: LoadAdError) {
-               // adError?.toString()?.let { Log.d(TAG, it) }
+                adError?.toString()?.let { Log.d(TAG, it) }
                 mInterstitialAd = null
             }
 
@@ -49,7 +66,9 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "Ad was loaded.")
                 mInterstitialAd = interstitialAd
             }
+
         })
+
 
         guessSubmit = findViewById(R.id.submitGuess)
         guessField = findViewById(R.id.guessField)
@@ -123,6 +142,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         private fun endGame() {
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
+
             guessField.isEnabled = false
             guessSubmit.isEnabled = false
             resetButton.visibility = View.VISIBLE
